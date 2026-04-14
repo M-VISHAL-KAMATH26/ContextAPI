41a0ba2425ba71ffb0db7882d67b516eea89dfc2
M-VISHAL-KAMATH26 <vishal.kamath@nineleaps.com>
2026-04-14 14:54:28 +0530

    learnt context api

---
+++ demoContext/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import UserContextProvider from './context/UserContextProvider';
+import Login from './components/Login';
+import Profile from './components/Profile';
+const App = () => {
+  return (
+    <UserContextProvider>
+      <h1>
+        react context demo
+      </h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
